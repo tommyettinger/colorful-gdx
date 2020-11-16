28e144af796f6df7b15cc33d7d072faf717beb53
Tommy Ettinger <tommy.ettinger@gmail.com>
2020-11-15 21:12:09 -0800

    Correct the Java code, not just GLSL, and verify round-trip

--- src/test/java/com/github/tommyettinger/colorful/ipt/SphereIPTWork.java
+++ src/test/java/com/github/tommyettinger/colorful/ipt/SphereIPTWork.java
@@ -16,11 +16,11 @@ public class SphereIPTWork {
         final float crMid = cyan.y * original.y + cyan.z * original.z;
         final float crScale = (original.x - 0.5f + (NumberUtils.floatToRawIntBits(crMid) >>> 31)) * cyan.x / -crMid;
         final float mgMid = magenta.y * original.y + magenta.z * original.z;
-        final float mgScale = (original.x - 0.5f + (NumberUtils.floatToRawIntBits(mgMid) >>> 31)) * magenta.x / -mgMid;
+        final float mgScale = (original.x + 0.5f - (NumberUtils.floatToRawIntBits(mgMid) >>> 31)) * magenta.x / -mgMid;
         final float ybMid = yellow.y * original.y + yellow.z * original.z;
         final float ybScale = (original.x - 0.5f + (NumberUtils.floatToRawIntBits(ybMid) >>> 31)) * yellow.x / -ybMid;
         final float scale = Math.max(crScale, Math.max(mgScale, ybScale));
-        final float d = MathUtils.cos(MathUtils.PI * original.x) * 0.5f / Vector2.len(original.y * scale, original.z * scale);
+        final float d = MathUtils.cos(3.14159f * original.x) * 0.5f / Vector2.len(original.y * scale, original.z * scale);
         return buffer.set(original.x, original.y * d, original.z * d);
     }
 
@@ -28,11 +28,11 @@ public class SphereIPTWork {
         final float crMid = cyan.y * original.y + cyan.z * original.z;
         final float crScale = (original.x - 0.5f + (NumberUtils.floatToRawIntBits(crMid) >>> 31)) * cyan.x / -crMid;
         final float mgMid = magenta.y * original.y + magenta.z * original.z;
-        final float mgScale = (original.x - 0.5f + (NumberUtils.floatToRawIntBits(mgMid) >>> 31)) * magenta.x / -mgMid;
+        final float mgScale = (original.x + 0.5f - (NumberUtils.floatToRawIntBits(mgMid) >>> 31)) * magenta.x / -mgMid;
         final float ybMid = yellow.y * original.y + yellow.z * original.z;
         final float ybScale = (original.x - 0.5f + (NumberUtils.floatToRawIntBits(ybMid) >>> 31)) * yellow.x / -ybMid;
         final float scale = Math.max(crScale, Math.max(mgScale, ybScale));
-        final float d = 2f * Vector2.len(original.y * scale, original.z * scale) / MathUtils.cos(MathUtils.PI * original.x);
+        final float d = 2f * Vector2.len(original.y * scale, original.z * scale) / MathUtils.cos(3.14159f * original.x);
         return buffer.set(original.x, original.y * d, original.z * d);
     }
 
@@ -53,22 +53,17 @@ public class SphereIPTWork {
         Vector3 kv = new Vector3(-0.5f, 0f, 0f);
         Vector3 wv = new Vector3(0.5f , 0f, 0f);
         Vector3 buffer = new Vector3(), buffer2 = new Vector3();
-        Vector3 testPoint = new Vector3(intensity(Palette.DRIFTWOOD) - 0.5f, protan(Palette.DRIFTWOOD) - 0.5f, tritan(Palette.DRIFTWOOD) - 0.5f);
-        System.out.println(testPoint);
-        System.out.println(plump(testPoint, buffer));
-        System.out.println(thin(buffer, buffer2));
-        testPoint.scl(-1f);
-        System.out.println(testPoint);
-        System.out.println(plump(testPoint, buffer));
-        System.out.println(thin(buffer, buffer2));
-        testPoint.set(intensity(Palette.FRESH_BLOOD) - 0.5f, protan(Palette.FRESH_BLOOD) - 0.5f, tritan(Palette.FRESH_BLOOD) - 0.5f);
-        System.out.println(testPoint);
-        System.out.println(plump(testPoint, buffer));
-        System.out.println(thin(buffer, buffer2));
-        testPoint.scl(-1f);
-        System.out.println(testPoint);
-        System.out.println(plump(testPoint, buffer));
-        System.out.println(thin(buffer, buffer2));
+        Vector3 testPoint = new Vector3();
+        for (int i = 0; i < 256; i++) {
+            String name = Palette.NAMES.get(i);
+            float color = Palette.NAMED.get(name, 0f);
+            if(ColorTools.alphaInt(color) < 128) continue;
+            testPoint.set(intensity(color) - 0.5f, protan(color) - 0.5f, tritan(color) - 0.5f);
+            plump(testPoint, buffer);
+            thin(buffer, buffer2);
+            if(!testPoint.epsilonEquals(buffer2, 0.01f))
+                System.out.printf("%s is bad! before: %s, after: %s\n", name, testPoint, buffer2);
+        }
 
 //        System.out.printf("public static final Vector3 yellow  = new Vector3%s;\n", (rv.cpy().sub(kv).crs(gv.cpy().sub(kv))));
 //        System.out.printf("public static final Vector3 magenta = new Vector3%s;\n", (rv.cpy().sub(kv).crs(bv.cpy().sub(kv))));
