9eee066789722191334a31321a9f95c0b395b55a
Tommy Ettinger <tommy.ettinger@gmail.com>
2020-11-27 23:47:16 -0800

    Change 2 color names and add ColorTools.randomColor() for IPT.
    Palette.FRESH_BLOOD is now RED, and Palette.RED is now BRIGHT_RED.

---
+++ src/test/java/com/github/tommyettinger/colorful/ipt/IPTRandomColorCheck.java
@@ -0,0 +1,17 @@
+package com.github.tommyettinger.colorful.ipt;
+
+import com.badlogic.gdx.math.RandomXS128;
+
+public class IPTRandomColorCheck {
+    public static void main(String[] args) {
+        RandomXS128 random = new RandomXS128(0L, 1L);
+        for (int i = 0; i < 10000; i++) {
+            float color = ColorTools.randomColor(random);
+            if(!ColorTools.inGamut(color)) {
+                System.out.printf("Color with IPT values: I=%f, P=%f, T=%f\n",
+                        ColorTools.intensity(color), ColorTools.protan(color), ColorTools.tritan(color));
+                System.out.println("It's out of gamut!");
+            }
+        }
+    }
+}
