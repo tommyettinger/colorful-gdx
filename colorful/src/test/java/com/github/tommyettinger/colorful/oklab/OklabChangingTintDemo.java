76c88a303cbe23b9c6ff1ae6f51d2b53b8eba1c9
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-01-22 20:07:22 -0800

    Add OklabChangingTintDemo.
    This seems to produce more saturated colors than the IPT_HQ version, probably because only a very small central column is especially desaturated.

---
+++ colorful/src/test/java/com/github/tommyettinger/colorful/oklab/OklabChangingTintDemo.java
@@ -0,0 +1,150 @@
+package com.github.tommyettinger.colorful.oklab;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3WindowAdapter;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
+import com.badlogic.gdx.math.RandomXS128;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.github.tommyettinger.colorful.FloatColors;
+import com.github.tommyettinger.colorful.Shaders;
+
+import static com.badlogic.gdx.Gdx.input;
+
+public class OklabChangingTintDemo extends ApplicationAdapter {
+//    public static final int SCREEN_WIDTH = 1531;
+//    public static final int SCREEN_HEIGHT = 862;
+    public static final int SCREEN_WIDTH = 808;
+    public static final int SCREEN_HEIGHT = 600;
+    protected SpriteBatch batch;
+    protected Viewport screenView;
+    protected Texture screenTexture;
+
+    private ShaderProgram defaultShader;
+    private ShaderProgram shader;
+    private float tint = Palette.GRAY, target = Palette.APPLE_GREEN;
+    private long seed = 1L;
+    private final RandomXS128 random = new RandomXS128(0xB0BAFE77L);
+
+    public static void main(String[] arg) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setTitle("Tint Demo");
+        config.setWindowedMode(SCREEN_WIDTH, SCREEN_HEIGHT);
+        config.setIdleFPS(10);
+        config.useVsync(true);
+
+        final OklabChangingTintDemo app = new OklabChangingTintDemo();
+        config.setWindowListener(new Lwjgl3WindowAdapter() {
+            @Override
+            public void filesDropped(String[] files) {
+                if (files != null && files.length > 0) {
+                    if (files[0].endsWith(".png") || files[0].endsWith(".jpg") || files[0].endsWith(".jpeg"))
+                        app.load(files[0]);
+                }
+            }
+        });
+
+        new Lwjgl3Application(app, config);
+    }
+
+    public void load(String name) {
+        //// loads a file by its full path, which we get via drag+drop
+        FileHandle file = Gdx.files.absolute(name);
+        if (!file.exists())
+        {
+            file = Gdx.files.classpath(name);
+            if(!file.exists())
+                return;
+        }
+        if (screenTexture != null) screenTexture.dispose();
+        screenTexture = new Texture(file);
+        screenTexture.setFilter(Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest);
+        int width, height;
+        Gdx.graphics.setWindowedMode(width = Math.min(screenTexture.getWidth() * 2, Gdx.graphics.getDisplayMode().width),
+                height = Math.min(screenTexture.getHeight(), Gdx.graphics.getDisplayMode().height));
+        screenView.update(width, height);
+        screenView.getCamera().position.set(width * 0.5f, height * 0.5f, 0f);
+        tint = Palette.GRAY;
+        seed = 1L;
+    }
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        defaultShader = SpriteBatch.createDefaultShader();
+        shader = new ShaderProgram(Shaders.vertexShader, Shaders.fragmentShaderOklab);
+        if (!shader.isCompiled()) throw new IllegalArgumentException("Error compiling shader: " + shader.getLog());
+        screenView = new ScreenViewport();
+        screenView.getCamera().position.set(SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f, 0);
+        screenView.update(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        batch.enableBlending();
+
+        // if you don't have these files on this absolute path, that's fine, and they will be ignored
+//        load("samples/Painting_by_Henri_Biva.jpg");
+//        load("samples/Among_the_Sierra_Nevada_by_Albert_Bierstadt.jpg");
+        load("samples/Mona_Lisa.jpg");
+    }
+
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(0.4f, 0.4f, 0.4f, 1f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        handleInput();
+        batch.setProjectionMatrix(screenView.getCamera().combined);
+        if (screenTexture != null) {
+            batch.setShader(shader);
+            float f = (seed * 0x1p-7f % 1f);
+            if(f > (f = ++seed * 0x1p-7f % 1f)) {
+                tint = target;
+                target = ColorTools.randomColor(random);
+            }
+            batch.setPackedColor(FloatColors.lerpFloatColors(tint, target, f));
+            batch.begin();
+            batch.draw(screenTexture, 0, 0);
+            batch.setShader(defaultShader);
+            batch.setPackedColor(-0x1.fffffep126f); // packed white
+            batch.draw(screenTexture, screenTexture.getWidth(), 0);
+            batch.end();
+        }
+        
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        screenView.update(width, height);
+        screenView.getCamera().position.set(width * 0.5f, height * 0.5f, 0f);
+        screenView.getCamera().update();
+    }
+
+    public void handleInput() {
+        if (input.isKeyPressed(Input.Keys.P)) // print
+            System.out.println("L=" + ColorTools.channelL(tint) + ",A=" + ColorTools.channelA(tint) + ",B=" + ColorTools.channelB(tint));
+        else if (input.isKeyPressed(Input.Keys.M))
+            load("samples/Mona_Lisa.jpg");
+        else if (input.isKeyPressed(Input.Keys.S)) //Sierra Nevada
+            load("samples/Among_the_Sierra_Nevada_by_Albert_Bierstadt.jpg");
+        else if (input.isKeyPressed(Input.Keys.B)) // Biva
+            load("samples/Painting_by_Henri_Biva.jpg");
+        else if (input.isKeyPressed(Input.Keys.C)) // Color Guard, pixel art cartoon-wargame style
+            load("samples/Color_Guard.png");
+        else if (input.isKeyPressed(Input.Keys.G)) // grayscale palette
+            load("samples/Grayscale_Spaceships.png");
+        else if (input.isKeyPressed(Input.Keys.A)) // higher-color atlas
+            load("samples/Spaceships.png");
+        else if (input.isKeyPressed(Input.Keys.R)) {
+            tint = Palette.GRAY;
+        }
+        else if (input.isKeyPressed(Input.Keys.Q) || input.isKeyPressed(Input.Keys.ESCAPE)) //quit
+            Gdx.app.exit();
+    }
+
+}
