9c1f28f3e5236c46be2c250adb4a829f6bfd34d9
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-05-10 02:46:06 -0700

    Attempt to order HALTONIC differently, not exactly a success.
    Former-commit-id: fe74c2239edb323be268517f4bd85306bd09340e

---
+++ colorful/src/test/java/com/github/tommyettinger/colorful/oklab/PaletteDifferentiator.java
@@ -0,0 +1,83 @@
+package com.github.tommyettinger.colorful.oklab;
+
+import com.badlogic.gdx.utils.IntArray;
+
+public class PaletteDifferentiator {
+    private static final int[] PALETTE = {
+            0x000000FF, 0xFFFFFFFF, 0x7B7B7BFF, 0x555555FF, 0xAAAAAAFF, 0x333333FF, 0xE0E0E0FF,
+            0xC8C8C8FF, 0xBEBB4EFF, 0x1FAE9AFF, 0xC2BBA9FF, 0xB46B58FF, 0x7C82C2FF, 0xF2825BFF, 0xD55193FF,
+            0x8C525CFF, 0x6AEF59FF, 0x1F439BFF, 0x793210FF, 0x3B3962FF, 0x16D72EFF, 0xB53FC6FF, 0xB380C7FF,
+            0xEDE389FF, 0x8420C6FF, 0x291710FF, 0x69D4D3FF, 0x76121CFF, 0x1FA92AFF, 0x64852CFF, 0x7A42DBFF,
+            0xEA5A5EFF, 0x7E3E8CFF, 0xB8FA35FF, 0x4F15DAFF, 0xBC3E61FF, 0xA19150FF, 0x9BBD25FF, 0xF095C2FF,
+            0xFFC24FFF, 0x7B7CFCFF, 0x9BE8C3FF, 0xE25EC4FF, 0x3D79ADFF, 0xC0422AFF, 0x260E5DFF, 0xF645A3FF,
+            0xF8ACE4FF, 0xB0871FFF, 0x42582CFF, 0x549787FF, 0xE31BA2FF, 0x1E222AFF, 0xB39CF5FF, 0x8C135FFF,
+            0x71CB92FF, 0xB767B3FF, 0x7E5030FF, 0x406697FF, 0x502B06FF, 0xDFAC73FF, 0xC21A26FF, 0xECFE65FF,
+            0x7E64E4FF, 0xBFD22EFF, 0xDA938FFF, 0x8E94E8FF, 0xA0DE92FF, 0x8C6BA9FF, 0x1662FCFF, 0xCA4EECFF,
+            0x8899AAFF, 0x24BC57FF, 0x680AA7FF, 0xFE6885FF, 0x2E1E6EFF, 0x875695FF, 0x981C20FF, 0x47723EFF,
+            0xF4E54FFF, 0x71174CFF, 0xC5F8ABFF, 0x75BFC7FF, 0xF23C37FF, 0xFC73E9FF, 0x893A5FFF, 0x4F50C5FF,
+            0xE06635FF, 0xB00D9FFF, 0xE90FCAFF, 0x1E9CFBFF, 0x3538F9FF, 0xE3971BFF, 0x500153FF, 0x2DB2CEFF,
+            0xB46D86FF, 0xFE43F2FF, 0x4FF990FF, 0x434531FF, 0xE31515FF, 0xDFA24BFF, 0x4282E6FF, 0x56626FFF,
+            0xF8B891FF, 0x4B0932FF, 0xD769E6FF, 0x906D1DFF, 0xD51144FF, 0x76B6F8FF, 0x4DF7ECFF, 0x169355FF,
+            0xB7C87DFF, 0x650C83FF, 0x0AE930FF, 0xEDB71AFF, 0x78AE77FF, 0x081236FF, 0x25E5F4FF, 0x5A4382FF,
+            0xB1FEFAFF, 0xEA7B0BFF, 0xF372C1FF, 0xA31479FF, 0x3EDB6AFF, 0xA44210FF, 0xB2C1FAFF, 0xAE9784FF,
+            0xE83175FF, 0xF925DFFF, 0xAB134FFF, 0xC03E83FF, 0x117F76FF, 0xE6E21DFF, 0x6B3858FF, 0x88ED12FF,
+            0x3E3486FF, 0x3DBB14FF, 0xD35521FF, 0xC2836DFF, 0x244E65FF, 0xAC29F6FF, 0xE71A58FF, 0x1127ABFF,
+            0xD086E0FF, 0x496B1CFF, 0xD27E96FF, 0x87353AFF, 0xD308EDFF, 0x5D3BAAFF, 0x11560BFF, 0x469AC6FF,
+            0xEDD4B9FF, 0xA4A222FF, 0x48A75CFF, 0xBB7213FF, 0xFBBAFAFF, 0x794811FF, 0x83804EFF, 0xB1FB85FF,
+            0x61C56DFF, 0x9D36B1FF, 0x201693FF, 0x184BB9FF, 0x5B0606FF, 0xAB5692FF, 0x090B23FF, 0xA7593AFF,
+            0x14D7ADFF, 0xAC6BF1FF, 0xCC0E7EFF, 0x1B90B4FF, 0xA5A94CFF, 0x264509FF, 0xE994FDFF, 0xC1E367FF,
+            0x1D16D5FF, 0x1C5C7DFF, 0xCF794CFF, 0xF6FF95FF, 0x7B1A88FF, 0x68B69CFF, 0xAADAF7FF, 0x6625E1FF,
+            0x223308FF, 0x7147FEFF, 0xDF6A7FFF, 0xF5FE22FF, 0xB6B1D2FF, 0x35E986FF, 0x2C69D4FF, 0x6D63C8FF,
+            0x32042DFF, 0xF4A293FF, 0x22040DFF, 0xF2FAC2FF, 0xFFBBB2FF, 0x9D3F7CFF, 0x86694EFF, 0xD34B57FF,
+            0x5B2E24FF, 0xF2CF80FF, 0x10EBAFFF, 0x7B603CFF, 0xFDE5A7FF, 0xB41808FF, 0xA83F4BFF, 0xC221B4FF,
+            0x9604A4FF, 0x878287FF, 0x3F1C16FF, 0x5AA7FEFF, 0x55096CFF, 0x1E9922FF, 0x031050FF, 0xA284A1FF,
+            0x2424EDFF, 0x8FD111FF, 0x480C8BFF, 0x71FE60FF, 0xFE1D02FF, 0xFF9A60FF, 0xD44ABEFF, 0xFE7B9AFF,
+            0x68915EFF, 0x9EFFD1FF, 0xABAC7CFF, 0x4413BFFF, 0xF93E83FF, 0x7A9633FF, 0xA05B73FF, 0x83A3C3FF,
+            0x124D4AFF, 0x397E0EFF, 0x6AFEB5FF, 0x975813FF, 0xFEC704FF, 0xBC1462FF, 0xA008E0FF, 0x418886FF,
+            0x58CAFEFF, 0x4E7A53FF, 0x7A07FFFF, 0x8D4EBCFF, 0xFE3257FF, 0xA46BD5FF, 0xB079FFFF, 0x909478FF,
+            0xFC6C42FF, 0x5F3342FF, 0x6A6A9DFF, 0xFF6315FF, 0x9D56D2FF, 0x6782A7FF, 0x957F24FF, 0xD08FB9FF,
+    };
+    private static final float[] OKLAB_PALETTE = new float[PALETTE.length];
+    static {
+        for (int i = 0; i < PALETTE.length; i++) {
+            OKLAB_PALETTE[i] = ColorTools.fromRGBA8888(PALETTE[i]);
+        }
+    }
+    public static double distance(final float c1, final float c2){
+        int i1 = Float.floatToRawIntBits(c1), i2 = Float.floatToRawIntBits(c2);
+        return Math.sqrt((((i1 & 0xff) - (i2 & 0xff)) * ((i1 & 0xff) - (i2 & 0xff)))
+                + ((((i1 >>> 8 & 0xff) - (i2 >>> 8 & 0xff)) * ((i1 >>> 8 & 0xff) - (i2 >>> 8 & 0xff))
+                + ((i1 >>> 16 & 0xff) - (i2 >>> 16 & 0xff)) * ((i1 >>> 16 & 0xff) - (i2 >>> 16 & 0xff))) * 4.0));
+    }
+    public static void main(String[] args){
+        IntArray next = new IntArray(255);
+        IntArray pull = new IntArray(255);
+        next.add(0);
+        for (int i = 1; i < 255; i++) {
+            pull.add(i);
+        }
+        for (int i = 1; i < 255; i++) {
+            double best = -1.0;
+            int bestIndex = -1;
+            for (int j = 0; j < pull.size; j++) {
+                double sum = 0.0;
+                float p = OKLAB_PALETTE[pull.get(j)];
+                for (int k = 0; k < next.size; k++) {
+                    sum += distance(p, OKLAB_PALETTE[next.get(k)]);
+                }
+                if(sum > best){
+                    best = sum;
+                    bestIndex = j;
+                }
+            }
+            next.add(pull.removeIndex(bestIndex));
+        }
+        System.out.printf("0x%08X, ", 0);
+        for (int i = 0, idx = 0; i < 32; i++) {
+            for (int j = i == 0 ? 1 : 0; j < 8; j++) {
+                System.out.printf("0x%08X, ", PALETTE[next.get(idx++)]);
+            }
+            System.out.println();
+        }
+    }
+}
