1ac3c9bb3654270f62156bc8a8664bdbddd948ba
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-05-15 22:12:04 -0700

    Now ColorSolidDemo is getting interesting.
    It shows YCwCm's color solid next to IPT's, which helps compare the two.
    
    
    Former-commit-id: a5417ce1e431b001b9a10f74db30a7573cc9378b

--- colorful/src/test/java/com/github/tommyettinger/colorful/ColorSolidDemo.java
+++ colorful/src/test/java/com/github/tommyettinger/colorful/ColorSolidDemo.java
@@ -25,14 +25,13 @@ public class ColorSolidDemo extends ApplicationAdapter {
 //    public static final int SCREEN_HEIGHT = 862;
     public static final int SCREEN_WIDTH = 512;
     public static final int SCREEN_HEIGHT = 512;
-    private ColorfulBatch batch;
-//    private SpriteBatch basicBatch;
+    private ColorfulBatch ycwcmBatch;
+    private com.github.tommyettinger.colorful.ipt.ColorfulBatch iptBatch;
     private Viewport screenView;
     private BitmapFont font;
     private Texture blank;
     private long lastProcessedTime = 0L, startTime;
     private float layer = 0.5f;
-    private float luma = 0.5f, warm = 0.5f, mild = 0.5f, contrast = 0.5f;
 
     public static void main(String[] arg) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
@@ -54,43 +53,76 @@ public class ColorSolidDemo extends ApplicationAdapter {
         blank = new Texture(b);
         font = new BitmapFont(Gdx.files.internal("font.fnt"));
         font.setColor(1f, 0.5f, 0.5f, 1f);
-        batch = new ColorfulBatch();
-        String vertexShader = batch.getShader().getVertexShaderSource();
-        String fragmentShader =
-                "#ifdef GL_ES\n" +
-                        "#define LOWP lowp\n" +
-                        "precision mediump float;\n" +
-                        "#else\n" +
-                        "#define LOWP \n" +
-                        "#endif\n" +
-                        "varying vec2 v_texCoords;\n" +
-                        "varying LOWP vec4 v_color;\n" +
-                        "varying LOWP vec4 v_tweak;\n" +
-                        "varying float v_lightFix;\n" +
-                        "uniform sampler2D u_texture;\n" +
-                        "const vec3 bright = vec3(0.375, 0.5, 0.125);\n" +
-                        "void main()\n" +
-                        "{\n" +
-                        "   vec4 tgt = texture2D( u_texture, v_texCoords );\n" +
-                        "   vec3 ycc = vec3(\n" +
-                        "     (v_color.r - 0.5 + v_tweak.r * pow(dot(tgt.rgb, bright), v_tweak.a) * v_lightFix),\n" + // luma
-                        "     (v_color.g - 0.5 + (tgt.r - tgt.b) * v_tweak.g) * 2.0,\n" + // warmth
-                        "     (v_color.b - 0.5 + (tgt.g - tgt.b) * v_tweak.b) * 2.0);\n" + // mildness
-                        "   gl_FragColor = vec4(\n" +
-                        "     dot(ycc, vec3(1.0, 0.625, -0.5)),\n" + // back to red
-                        "     dot(ycc, vec3(1.0, -0.375, 0.5)),\n" + // back to green
-                        "     dot(ycc, vec3(1.0, -0.375, -0.5)),\n" + // back to blue
-                        "     v_color.a * tgt.a);\n" + // back to alpha and clamp
-                        "   if(any(notEqual(clamp(gl_FragColor.rgb, 0.0, 1.0), gl_FragColor.rgb))) discard;\n" +
-                        "}";
-        ShaderProgram shader = new ShaderProgram(vertexShader, fragmentShader);
-        if (!shader.isCompiled()) throw new IllegalArgumentException("Error compiling shader: " + shader.getLog());
-        batch.setShader(shader);
-//        basicBatch = new SpriteBatch();
+        ycwcmBatch = new ColorfulBatch();
+        {
+            String vertexShader = ycwcmBatch.getShader().getVertexShaderSource();
+            String fragmentShader =
+                    "#ifdef GL_ES\n" +
+                            "#define LOWP lowp\n" +
+                            "precision mediump float;\n" +
+                            "#else\n" +
+                            "#define LOWP \n" +
+                            "#endif\n" +
+                            "varying vec2 v_texCoords;\n" +
+                            "varying LOWP vec4 v_color;\n" +
+                            "varying LOWP vec4 v_tweak;\n" +
+                            "varying float v_lightFix;\n" +
+                            "uniform sampler2D u_texture;\n" +
+                            "const vec3 bright = vec3(0.375, 0.5, 0.125);\n" +
+                            "void main()\n" +
+                            "{\n" +
+                            "   vec4 tgt = texture2D( u_texture, v_texCoords );\n" +
+                            "   vec3 ycc = vec3(\n" +
+                            "     (v_color.r - 0.5 + v_tweak.r * pow(dot(tgt.rgb, bright), v_tweak.a) * v_lightFix),\n" + // luma
+                            "     (v_color.g - 0.5 + (tgt.r - tgt.b) * v_tweak.g) * 2.0,\n" + // warmth
+                            "     (v_color.b - 0.5 + (tgt.g - tgt.b) * v_tweak.b) * 2.0);\n" + // mildness
+                            "   gl_FragColor = vec4(\n" +
+                            "     dot(ycc, vec3(1.0, 0.625, -0.5)),\n" + // back to red
+                            "     dot(ycc, vec3(1.0, -0.375, 0.5)),\n" + // back to green
+                            "     dot(ycc, vec3(1.0, -0.375, -0.5)),\n" + // back to blue
+                            "     v_color.a * tgt.a);\n" + // back to alpha and clamp
+                            "   if(any(notEqual(clamp(gl_FragColor.rgb, 0.0, 1.0), gl_FragColor.rgb))) discard;\n" +
+                            "}";
+            ShaderProgram shader = new ShaderProgram(vertexShader, fragmentShader);
+
+            if (!shader.isCompiled()) throw new IllegalArgumentException("Error compiling shader: " + shader.getLog());
+            ycwcmBatch.setShader(shader);
+        }
+        iptBatch = new com.github.tommyettinger.colorful.ipt.ColorfulBatch();
+        {
+            String vertexShader = iptBatch.getShader().getVertexShaderSource();
+            String fragmentShader =
+                    "#ifdef GL_ES\n" +
+                            "#define LOWP lowp\n" +
+                            "precision mediump float;\n" +
+                            "#else\n" +
+                            "#define LOWP \n" +
+                            "#endif\n" +
+                            "varying vec2 v_texCoords;\n" +
+                            "varying LOWP vec4 v_color;\n" +
+                            "varying LOWP vec4 v_tweak;\n" +
+                            "varying float v_lightFix;\n" +
+                            "uniform sampler2D u_texture;\n" +
+                            "void main()\n" +
+                            "{\n" +
+                            "  vec4 tgt = texture2D( u_texture, v_texCoords );\n" +
+                            "  vec3 ipt = mat3(0.189786, 0.669665 , 0.286498, 0.576951, -0.73741 , 0.655205, 0.233221, 0.0681367, -0.941748)\n" +
+                            "             * tgt.rgb;\n" +
+                            "  ipt.x = pow(ipt.x, v_tweak.a) * v_lightFix * v_tweak.r + v_color.r - 0.5;\n" +
+                            "  ipt.yz = (ipt.yz * v_tweak.gb + v_color.gb - 0.5) * 2.0;\n" +
+                            "  vec3 back = mat3(0.999779, 1.00015, 0.999769, 1.07094, -0.377744, 0.0629496, 0.324891, 0.220439, -0.809638) * ipt;\n" +
+                            "  gl_FragColor = vec4(back, v_color.a * tgt.a);\n" +
+                            "  if(any(notEqual(clamp(gl_FragColor.rgb, 0.0, 1.0), gl_FragColor.rgb))) discard;\n" +
+                            "}";
+            ShaderProgram shader = new ShaderProgram(vertexShader, fragmentShader);
+
+            if (!shader.isCompiled()) throw new IllegalArgumentException("Error compiling shader: " + shader.getLog());
+            iptBatch.setShader(shader);
+        }
+
         screenView = new ScreenViewport();
         screenView.getCamera().position.set(SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f, 0);
         screenView.update(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        batch.enableBlending();
     }
 
 
@@ -100,17 +132,29 @@ public class ColorSolidDemo extends ApplicationAdapter {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         handleInput();
         layer = TrigTools.acos_(TrigTools.sin_(TimeUtils.timeSinceMillis(startTime) * 0x1p-13f)) * 2f;
-        batch.setProjectionMatrix(screenView.getCamera().combined);
-        batch.setPackedColor(Palette.GRAY);
-        batch.begin();
-        batch.setTweak(luma, warm, mild, contrast);
-        batch.draw(blank, 0, 0, 512, 512);
-        for (int x = 0; x < 512; x++) {
-            for (int y = 0; y < 512; y++) {
-                batch.setColor(layer, x * 0x1p-9f, y * 0x1p-9f, 1f);
-                batch.draw(blank, x, y, 1f, 1f);
+        ycwcmBatch.setProjectionMatrix(screenView.getCamera().combined);
+        ycwcmBatch.setPackedColor(Palette.GRAY);
+        ycwcmBatch.begin();
+        ycwcmBatch.draw(blank, 0, 0, 512, 512);
+        for (int x = 0; x < 256; x++) {
+            for (int y = 0; y < 256; y++) {
+                ycwcmBatch.setColor(layer, x * 0x1p-8f, y * 0x1p-8f, 1f);
+                ycwcmBatch.draw(blank, x, y + 256f, 1f, 1f);
             }
         }
+        ycwcmBatch.end();
+        iptBatch.setProjectionMatrix(screenView.getCamera().combined);
+        iptBatch.begin();
+        for (int x = 0; x < 256; x++) {
+            for (int y = 0; y < 256; y++) {
+                iptBatch.setColor(layer, x * 0x1p-8f, y * 0x1p-8f, 1f);
+                iptBatch.draw(blank, x + 256f, y + 256f, 1f, 1f);
+            }
+        }
+        iptBatch.end();
+
+
+
 //        final float maxDist = 254f * TrigTools.sin_(layer * 0.5f) + 1f, iMax = 1f / maxDist;
 //        final float circumference = MathUtils.PI * 511f;
 //        batch.setPackedColor(ColorTools.ycwcm(layer, 0.5f, 0.5f, 1f));
@@ -127,7 +171,6 @@ public class ColorSolidDemo extends ApplicationAdapter {
 //                batch.draw(blank, 254.75f + x * dist, 254.75f + y * dist, 1.5f, 1.5f);
 //            }
 //        }
-        batch.end();
     }
 
     @Override
@@ -150,32 +193,7 @@ public class ColorSolidDemo extends ApplicationAdapter {
             {
                 layer = MathUtils.random();
             } else if (input.isKeyPressed(Input.Keys.P)) // print
-                System.out.println("Using layer=" + layer
-                        +", and using tweak with luma="+luma
-                        + ",warm="+warm + ",mild="+mild+",contrast="+contrast + " .");
-            else if (input.isKeyPressed(Input.Keys.L)) //light
-                luma = MathUtils.clamp(luma + 0x3p-7f, 0f, 1f);
-            else if (input.isKeyPressed(Input.Keys.D)) //dark
-                luma = MathUtils.clamp(luma - 0x3p-7f, 0f, 1f);
-            else if (input.isKeyPressed(Input.Keys.W)) //warm
-                warm = MathUtils.clamp(warm + 0x3p-7f, 0f, 1f);
-            else if (input.isKeyPressed(Input.Keys.C)) //cool
-                warm = MathUtils.clamp(warm - 0x3p-7f, 0f, 1f);
-            else if (input.isKeyPressed(Input.Keys.M)) //mild
-                mild = MathUtils.clamp(mild + 0x3p-7f, 0f, 1f);
-            else if (input.isKeyPressed(Input.Keys.B)) //bold
-                mild = MathUtils.clamp(mild - 0x3p-7f, 0f, 1f);
-            else if (input.isKeyPressed(Input.Keys.S)) //sharp contrast
-                contrast = MathUtils.clamp(contrast + 0x3p-7f, 0f, 1f);
-            else if (input.isKeyPressed(Input.Keys.F)) //fuzzy contrast
-                contrast = MathUtils.clamp(contrast - 0x3p-7f, 0f, 1f);
-            else if (input.isKeyPressed(Input.Keys.BACKSPACE)) //reset
-            {
-                luma = 0.5f;
-                warm = 0.5f;
-                mild = 0.5f;
-                contrast = 0.5f;
-            }
+                System.out.println("Using layer=" + layer + ".");
         }
     }
 }
