9adeb023879642333c91e8a49cbe4c6afbbb0ecf
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-09-19 18:27:12 -0700

    Kinda-automatically name Yam palette colors.
    Former-commit-id: 42814e84323605205a03fd2656eb1915d6aff5c1

--- colorful/src/test/java/com/github/tommyettinger/colorful/oklab/YamPaletteGenerator.java
+++ colorful/src/test/java/com/github/tommyettinger/colorful/oklab/YamPaletteGenerator.java
@@ -5,6 +5,10 @@ import com.badlogic.gdx.utils.FloatArray;
 import com.github.tommyettinger.colorful.TrigTools;
 import com.github.tommyettinger.colorful.internal.StringKit;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 import static com.badlogic.gdx.math.MathUtils.lerp;
 import static com.github.tommyettinger.colorful.oklab.ColorTools.*;
 import static com.github.tommyettinger.colorful.oklab.SimplePalette.*;
@@ -48,40 +52,79 @@ public class YamPaletteGenerator {
                 oklabHue(PURPLE),
                 oklabHue(MAGENTA),
         }, hueKeys;
+        String[] hueNames = new String[]{
+                "red",
+                "brown",
+                "orange",
+                "saffron",
+                "yellow",
+                "lime",
+                "green",
+                "cyan",
+                "blue",
+                "violet",
+                "purple",
+                "magenta",
+        }, nameKeys, levelNames;
         FloatArray pal = new FloatArray(256);
+        ArrayList<String> names = new ArrayList<>(256);
         pal.add(TRANSPARENT);
+        names.add("transparent");
         for (int i = 0; i < 15; i++) {
             pal.add(oklabByHCL(0.1f, 0.01f, i / 14f, 1f));
         }
+        Collections.addAll(names, "pure black", "almost black", "lead black",
+                "black lead", "pure lead", "gray lead",
+                "lead gray", "pure gray", "silver gray",
+                "gray silver", "pure silver", "white silver",
+                "silver white", "almost white", "pure white");
         for (int wave = 1; wave <= 4; wave++) {
             int crest = 5 - wave;
             switch (wave){
                 case 1:
                     hueKeys = new float[12];
                     System.arraycopy(coreHues, 0, hueKeys, 0, 12);
+                    nameKeys = new String[12];
+                    System.arraycopy(hueNames, 0, nameKeys, 0, 12);
+                    levelNames = new String[]{"black ", "lead ", "silver ", "white "};
                     break;
                 case 2:
                     hueKeys = new float[24];
-                    for (int i = 0, c = 0; i < 12; i++) {
-                        hueKeys[c++] = lerpAngle_(coreHues[i], coreHues[(i+1)%12], 1f/4f);
-                        hueKeys[c++] = lerpAngle_(coreHues[i], coreHues[(i+1)%12], 3f/4f);
+                    nameKeys = new String[24];
+                    levelNames = new String[]{"drab ", "dull ", "pale "};
+                    for (int i = 0, c = -1; i < 12; i++) {
+                        hueKeys[++c] = lerpAngle_(coreHues[i], coreHues[(i+1)%12], 1f/4f);
+                        nameKeys[c] = hueNames[(i+1)%12] + ' ' + hueNames[i];
+                        hueKeys[++c] = lerpAngle_(coreHues[i], coreHues[(i+1)%12], 3f/4f);
+                        nameKeys[c] = hueNames[i] + ' ' + hueNames[(i+1)%12];
                     }
                     break;
                 case 3:
                     hueKeys = new float[36];
-                    for (int i = 0, c = 0; i < 12; i++) {
-                        hueKeys[c++] = lerpAngle_(coreHues[i], coreHues[(i+1)%12], 0f);
-                        hueKeys[c++] = lerpAngle_(coreHues[i], coreHues[(i+1)%12], 1f/3f);
-                        hueKeys[c++] = lerpAngle_(coreHues[i], coreHues[(i+1)%12], 2f/3f);
+                    nameKeys = new String[36];
+                    levelNames = new String[]{"deep ", "bright "};
+                    for (int i = 0, c = -1; i < 12; i++) {
+                        hueKeys[++c] = lerpAngle_(coreHues[i], coreHues[(i+1)%12], 0f);
+                        nameKeys[c] = hueNames[i];
+                        hueKeys[++c] = lerpAngle_(coreHues[i], coreHues[(i+1)%12], 1f/3f);
+                        nameKeys[c] = hueNames[(i+1)%12] + ' ' + hueNames[i];
+                        hueKeys[++c] = lerpAngle_(coreHues[i], coreHues[(i+1)%12], 2f/3f);
+                        nameKeys[c] = hueNames[i] + ' ' + hueNames[(i+1)%12];
                     }
                     break;
                 default:
                     hueKeys = new float[48];
-                    for (int i = 0, c = 0; i < 12; i++) {
-                        hueKeys[c++] = lerpAngle_(coreHues[i], coreHues[(i+1)%12], 1f/8f);
-                        hueKeys[c++] = lerpAngle_(coreHues[i], coreHues[(i+1)%12], 3f/8f);
-                        hueKeys[c++] = lerpAngle_(coreHues[i], coreHues[(i+1)%12], 5f/8f);
-                        hueKeys[c++] = lerpAngle_(coreHues[i], coreHues[(i+1)%12], 7f/8f);
+                    nameKeys = new String[48];
+                    levelNames = new String[]{""};
+                    for (int i = 0, c = -1; i < 12; i++) {
+                        hueKeys[++c] = lerpAngle_(coreHues[i], coreHues[(i+1)%12], 1f/8f);
+                        nameKeys[c] = "some-" + hueNames[(i+1)%12] + ' ' + hueNames[i];
+                        hueKeys[++c] = lerpAngle_(coreHues[i], coreHues[(i+1)%12], 3f/8f);
+                        nameKeys[c] = "more-" + hueNames[(i+1)%12] + ' ' + hueNames[i];
+                        hueKeys[++c] = lerpAngle_(coreHues[i], coreHues[(i+1)%12], 5f/8f);
+                        nameKeys[c] = "more-" + hueNames[i] + ' ' + hueNames[(i+1)%12];
+                        hueKeys[++c] = lerpAngle_(coreHues[i], coreHues[(i+1)%12], 7f/8f);
+                        nameKeys[c] = "some-" + hueNames[i] + ' ' + hueNames[(i+1)%12];
                     }
                     break;
             }
@@ -114,6 +157,7 @@ public class YamPaletteGenerator {
                 float maxL = (maxLight / 255f) * lightAdjust;
                 for (int j = 0, cr = 1; j < crest; j++, cr += 2) {
                     pal.add(oklabByHCL(hue, outerC * (float) Math.pow(quart, 1.375f), lerp(minL, maxL, cr / (crest * 2f)), 1f));
+                    names.add(levelNames[j] + nameKeys[i]);
                 }
             }
         }
@@ -124,18 +168,23 @@ public class YamPaletteGenerator {
             if(7 == (i & 7)) sb.append('\n');
         }
 
-        sb.append("}\n\nOklab:\n{\n");
-        for (int i = 0; i < pal.size; i++) {
-            sb.append("0x00");
-            StringKit.appendHex(sb, (byte) (ColorTools.channelL(pal.get(i)) * 255));
+        sb.append("}\n\nOklab:\n{\n0x00808000, ");
+        for (int i = 1; i < pal.size; i++) {
+            sb.append("0xFF");
             float a = ((ColorTools.channelA(pal.get(i)) - 0.5f) * 255 * 3.25f + 127.5f);
             float b = ((ColorTools.channelB(pal.get(i)) - 0.5f) * 255 * 3.25f + 127.5f);
             if(a <= -1 || a >= 256) System.out.println(a + " is bad");
             if(b <= -1 || b >= 256) System.out.println(b + " is bad");
-            StringKit.appendHex(sb, (byte) a);
             StringKit.appendHex(sb, (byte) b);
+            StringKit.appendHex(sb, (byte) a);
+            StringKit.appendHex(sb, (byte) (ColorTools.channelL(pal.get(i)) * 255));
             sb.append(", ");
             if(7 == (i & 7)) sb.append('\n');
+        }
+
+        sb.append("}\n\nNamed:\n{\n");
+        for (int i = 0; i < pal.size; i++) {
+            sb.append(names.get(i)).append(" :: ").append(Float.toHexString(pal.get(i))).append(",\n");
         }
 
         System.out.println(sb.append('}'));
