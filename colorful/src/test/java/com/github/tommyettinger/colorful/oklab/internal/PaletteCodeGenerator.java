1c210df8238e8905b1bd1b88f1e19a89636f6a5b
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-11-06 21:36:43 -0700

    Regenerate FullPalette, docs.
    Former-commit-id: 6c104e3b42ddb6a15eca54205058b65c82eece73

--- colorful/src/test/java/com/github/tommyettinger/colorful/oklab/internal/PaletteCodeGenerator.java
+++ colorful/src/test/java/com/github/tommyettinger/colorful/oklab/internal/PaletteCodeGenerator.java
@@ -21,17 +21,17 @@ import static com.github.tommyettinger.colorful.oklab.ColorTools.toRGBA8888;
  * Created by Tommy Ettinger on 12/8/2019.
  */
 public class PaletteCodeGenerator extends ApplicationAdapter {
-    public static final String outputAdd = "Oklab";
-    public static final String inputName = "AuroraColorData.txt";
-    public static final ObjectFloatMap<String> named = Palette.NAMED;
+//    public static final String outputAdd = "Oklab";
+//    public static final String inputName = "AuroraColorData.txt";
+//    public static final ObjectFloatMap<String> named = Palette.NAMED;
 
 //    public static final String outputAdd = "YamOklab";
 //    public static final String inputName = "YamColorData.txt";
 //    public static final ObjectFloatMap<String> named = YamPalette.NAMED;
 
-//    public static final String outputAdd = "FullOklab";
-//    public static final String inputName = "ColorData.txt";
-//    public static final ObjectFloatMap<String> named = FullPalette.NAMED;
+    public static final String outputAdd = "FullOklab";
+    public static final String inputName = "ColorData.txt";
+    public static final ObjectFloatMap<String> named = FullPalette.NAMED;
 
 //    public static final String outputAdd = "NamedMunsellOklab";
 //    public static final String inputName = "ISCCNBSData.txt";
