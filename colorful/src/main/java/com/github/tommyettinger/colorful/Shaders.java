0617a0e3040537503b4146b262b18d05e8981072
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-11-13 18:36:43 -0800

    Add two color-inverting shaders; tests up next.
    Former-commit-id: 6056458d6c36223feef8c6fd2dbfad676b155b05

--- colorful/src/main/java/com/github/tommyettinger/colorful/Shaders.java
+++ colorful/src/main/java/com/github/tommyettinger/colorful/Shaders.java
@@ -1321,5 +1321,63 @@ void main()
                     "                 (base * base * base)," +
                     "                 0.0, 1.0)), v_color.a * tgt.a);\n" +
                     "}";
+    /**
+     * The simplest possible color-inverting shader for a SpriteBatch, this just takes each RGB channel and subtracts it
+     * from 1.0 to get the value this uses. This uses an RGBA batch color.
+     * <br>
+     * You can generate RGB colors using any of various methods in the {@code rgb} package, such as
+     * {@link com.github.tommyettinger.colorful.rgb.ColorTools#rgb(float, float, float, float)}.
+     * <br>
+     * Meant only for use with {@link #vertexShader}.
+     */
+    public static final String fragmentShaderInvertedRGB =
+            "#ifdef GL_ES\n" +
+                    "#define LOWP lowp\n" +
+                    "precision mediump float;\n" +
+                    "#else\n" +
+                    "#define LOWP \n" +
+                    "#endif\n" +
+                    "varying vec2 v_texCoords;\n" +
+                    "varying LOWP vec4 v_color;\n" +
+                    "uniform sampler2D u_texture;\n" +
+                    "void main()\n" +
+                    "{\n" +
+                    "   vec4 tgt = texture2D(u_texture, v_texCoords);\n" +
+                    "   gl_FragColor = vec4(1.0 - tgt.rgb * v_color.rgb, v_color.a * tgt.a);\n" +
+                    "}";
+
+    /**
+     * Just like {@link #fragmentShaderInvertedRGB}, but internally converts to Oklab, so it can invert just lightness
+     * without changing color hue or saturation. This uses an RGBA batch color.
+     * <br>
+     * You can generate RGB colors using any of various methods in the {@code rgb} package, such as
+     * {@link com.github.tommyettinger.colorful.rgb.ColorTools#rgb(float, float, float, float)}.
+     * <br>
+     * Meant for use with {@link #vertexShader}.
+     */
+    public static String fragmentShaderInvertedLightness =
+            "#ifdef GL_ES\n" +
+                    "#define LOWP lowp\n" +
+                    "precision mediump float;\n" +
+                    "#else\n" +
+                    "#define LOWP \n" +
+                    "#endif\n" +
+                    "varying vec2 v_texCoords;\n" +
+                    "varying LOWP vec4 v_color;\n" +
+                    "uniform sampler2D u_texture;\n" +
+                    "const vec3 forward = vec3(1.0 / 3.0);\n" +
+                    "void main()\n" +
+                    "{\n" +
+                    "  vec4 tgt = texture2D( u_texture, v_texCoords ) * v_color;\n" +
+                    "  vec3 lab = mat3(+0.2104542553, +1.9779984951, +0.0259040371, +0.7936177850, -2.4285922050, +0.7827717662, -0.0040720468, +0.4505937099, -0.8086757660) *" +
+                    "             pow(mat3(0.4121656120, 0.2118591070, 0.0883097947, 0.5362752080, 0.6807189584, 0.2818474174, 0.0514575653, 0.1074065790, 0.6302613616) \n" +
+                    "             * (tgt.rgb * tgt.rgb), forward);\n" +
+                    "  lab.x = 1.0 - lab.x;\n" +
+                    "  lab = mat3(1.0, 1.0, 1.0, +0.3963377774, -0.1055613458, -0.0894841775, +0.2158037573, -0.0638541728, -1.2914855480) * lab;\n" +
+                    "  gl_FragColor = vec4(sqrt(clamp(" +
+                    "                 mat3(+4.0767245293, -1.2681437731, -0.0041119885, -3.3072168827, +2.6093323231, -0.7034763098, +0.2307590544, -0.3411344290, +1.7068625689) *\n" +
+                    "                 (lab * lab * lab)," +
+                    "                 0.0, 1.0)), tgt.a);\n" +
+                    "}";
 
 }
