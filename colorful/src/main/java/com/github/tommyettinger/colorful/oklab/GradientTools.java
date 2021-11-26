e722080ca06ebf7a04d82719c892935de691e6d1
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-11-25 21:38:37 -0800

    Split makeGradient() into appendGradient() and appendPartialGradient().
    The last of these methods should be useful for full customization of chained gradients.
    
    
    Former-commit-id: 53b571e5ba4fa19ee38aaf51493f45f20a5fd737

--- colorful/src/main/java/com/github/tommyettinger/colorful/oklab/GradientTools.java
+++ colorful/src/main/java/com/github/tommyettinger/colorful/oklab/GradientTools.java
@@ -4,6 +4,10 @@ import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.utils.FloatArray;
 import com.github.tommyettinger.colorful.FloatColors;
 
+/**
+ * Static methods for handling gradients of smoothly-changing colors, typically inside of {@link FloatArray}s.
+ * This class does some special handling for Oklab colors.
+ */
 public class GradientTools {
     /**
      * No need to instantiate.
@@ -23,7 +27,7 @@ public class GradientTools {
      * @param interpolation a libGDX Interpolation that can be used to customize how start transitions to end.
      * @return {@code appending}, after adding the gradient to the end
      */
-    public static FloatArray makeGradient(FloatArray appending, float start, float end, int steps, Interpolation interpolation){
+    public static FloatArray appendGradient(FloatArray appending, float start, float end, int steps, Interpolation interpolation) {
         if(appending == null)
             return null;
         if(steps <= 0) {
@@ -34,9 +38,35 @@ public class GradientTools {
             return appending;
         }
         appending.ensureCapacity(steps);
+        appendPartialGradient(appending, start, end, steps - 1, interpolation).add(end);
+        return appending;
+    }
+
+    /**
+     * Exactly like {@link #appendGradient(FloatArray, float, float, int, Interpolation)}, but does not include
+     * {@code end} in what it appends to {@code appending}. This is intended for the implementation of chained
+     * gradients, where the end of a previous gradient becomes the start of the next one. This still uses the specified
+     * number of steps, it just doesn't append {@code end} in the last step.
+     * @param appending a FloatArray that will be appended to
+     * @param start the packed float Oklab color to start with
+     * @param end the packed float Oklab color to end just before
+     * @param steps how many steps the gradient should use; usually greater than 2
+     * @param interpolation a libGDX Interpolation that can be used to customize how start transitions toward end.
+     * @return {@code appending}, after adding the gradient to its end
+     */
+    public static FloatArray appendPartialGradient(FloatArray appending, float start, float end, int steps, Interpolation interpolation){
+        if(appending == null)
+            return null;
+        if(steps <= 0) {
+            return appending;
+        }
+        if(steps == 1) {
+            appending.add(start);
+            return appending;
+        }
         int limit = steps - 1;
-        float step = 1f / limit, change = 0f;
-        for (int i = 0; i <= limit; i++) {
+        float step = 1f / steps, change = 0f;
+        for (int i = 0; i < limit; i++) {
             appending.add(ColorTools.limitToGamut(FloatColors.lerpFloatColors(start, end, interpolation.apply(change))));
             change += step;
         }
