d8134b0d085fd485d3ab6cf096da930f48605cc2
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-11-25 22:17:51 -0800

    I think this works; appendGradientChain allows using an Interpolation for many colors.
    Former-commit-id: defd5aef5c86232e4a39949932b646def236c79d

--- colorful/src/main/java/com/github/tommyettinger/colorful/oklab/GradientTools.java
+++ colorful/src/main/java/com/github/tommyettinger/colorful/oklab/GradientTools.java
@@ -1,6 +1,7 @@
 package com.github.tommyettinger.colorful.oklab;
 
 import com.badlogic.gdx.math.Interpolation;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.utils.FloatArray;
 import com.github.tommyettinger.colorful.FloatColors;
 
@@ -36,7 +37,7 @@ public class GradientTools {
      * @param start the packed float Oklab color to start with
      * @param end the packed float Oklab color to end on
      * @param steps how many steps the gradient should use; usually greater than 2, and must be non-negative
-     * @param interpolation a libGDX Interpolation that can be used to customize how start transitions to end.
+     * @param interpolation a libGDX Interpolation that can be used to customize how start transitions to end
      * @return a new FloatArray that contains the requested gradient
      */
     public static FloatArray makeGradient(float start, float end, int steps, Interpolation interpolation) {
@@ -61,7 +62,7 @@ public class GradientTools {
      * @param start the packed float Oklab color to start with
      * @param end the packed float Oklab color to end on
      * @param steps how many steps the gradient should use; usually greater than 2
-     * @param interpolation a libGDX Interpolation that can be used to customize how start transitions to end.
+     * @param interpolation a libGDX Interpolation that can be used to customize how start transitions to end
      * @return {@code appending}, after adding the gradient to the end
      */
     public static FloatArray appendGradient(FloatArray appending, float start, float end, int steps, Interpolation interpolation) {
@@ -80,6 +81,42 @@ public class GradientTools {
     }
 
     /**
+     * Appends a gradient between several packed float Oklab colors provided in {@code chain}. This uses the specified
+     * Interpolation for the whole gradient, which can make some colors use smaller sections than others. Appends to the
+     * end of {@code appending} and produces a total of {@code steps} colors.
+     * @param appending a FloatArray that will be appended to
+     * @param steps how many steps the gradient should use; usually greater than 2
+     * @param interpolation a libGDX Interpolation that can be used to customize how start transitions to end
+     * @param chain an array or varargs of packed float Oklab colors that this will interpolate through in order
+     * @return {@code appending}, after adding the gradient to the end
+     */
+    public static FloatArray appendGradientChain(FloatArray appending, int steps, Interpolation interpolation, float... chain) {
+        if (appending == null)
+            return null;
+        if(chain == null)
+            return appending;
+        if (steps <= 0 || chain.length == 0) {
+            return appending;
+        }
+        if (steps == 1 || chain.length == 1) {
+            appending.add(chain[0]);
+            return appending;
+        }
+        appending.ensureCapacity(steps);
+        int limit = steps - 1, splits = chain.length - 1;
+        float step = 1f / steps, change = 0f;
+        for (int i = 0; i < limit; i++) {
+            float interp = interpolation.apply(change);
+            float splint = Math.min(Math.max(interp * splits, 0f), splits - 0.000001f);
+            int idx = (int)splint;
+            appending.add(ColorTools.limitToGamut(FloatColors.lerpFloatColors(chain[idx], chain[idx+1], MathUtils.norm(idx * splits, idx * splits + splits, splint))));
+            change += step;
+        }
+        appending.add(chain[splits]);
+        return appending;
+    }
+
+    /**
      * Exactly like {@link #appendGradient(FloatArray, float, float, int, Interpolation)}, but does not include
      * {@code end} in what it appends to {@code appending}. This is intended for the implementation of chained
      * gradients, where the end of a previous gradient becomes the start of the next one. This still uses the specified
@@ -88,7 +125,7 @@ public class GradientTools {
      * @param start the packed float Oklab color to start with
      * @param end the packed float Oklab color to end just before
      * @param steps how many steps the gradient should use; usually greater than 2
-     * @param interpolation a libGDX Interpolation that can be used to customize how start transitions toward end.
+     * @param interpolation a libGDX Interpolation that can be used to customize how start transitions toward end
      * @return {@code appending}, after adding the gradient to its end
      */
     public static FloatArray appendPartialGradient(FloatArray appending, float start, float end, int steps, Interpolation interpolation){
