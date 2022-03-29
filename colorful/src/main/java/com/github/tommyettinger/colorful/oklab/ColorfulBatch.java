483605703de5dfa023b7a5b77dd80692e995a500
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-03-28 22:13:12 -0700

    Update Oklab ColorfulBatch shader.
    I'm pretty sure this works, but not 100%. There's some odd behavior in a test currently, and I think the palettes may need to be rewritten.
    
    
    Former-commit-id: e3ad3e2bf2ff41ccc0029f1d8b48e13f616a5ca8

--- colorful/src/main/java/com/github/tommyettinger/colorful/oklab/ColorfulBatch.java
+++ colorful/src/main/java/com/github/tommyettinger/colorful/oklab/ColorfulBatch.java
@@ -178,11 +178,27 @@ public class ColorfulBatch implements Batch {
                         "uniform sampler2D u_texture;\n" +
                         "const vec3 forward = vec3(1.0 / 3.0);\n" +
                         "float toOklab(float L) {\n" +
-                        "  return (L - 1.0) / (1.0 - L * 0.4285714) + 1.0;\n" +
+                        "        const float shape = 0.64516133, turning = 0.95;\n" +
+                        "        float d = turning - L;\n" +
+                        "        return mix(\n" +
+                        "          ((1. - turning) * (L - 1.)) / (1. - (L + shape * d)) + 1.,\n" +
+                        "          (turning * L) / (1.0e-20 + (L + shape * d)),\n" +
+                        "          step(0.0, d));\n" +
                         "}\n" +
                         "float fromOklab(float L) {\n" +
-                        "  return (L - 1.0) / (1.0 + L * 0.75) + 1.0;\n" +
+                        "        const float shape = 1.55, turning = 0.95;\n" +
+                        "        float d = turning - L;\n" +
+                        "        return mix(\n" +
+                        "          ((1. - turning) * (L - 1.)) / (1. - (L + shape * d)) + 1.,\n" +
+                        "          (turning * L) / (1.0e-20 + (L + shape * d)),\n" +
+                        "          step(0.0, d));\n" +
                         "}\n" +
+//                        "float toOklab(float L) {\n" +
+//                        "  return (L - 1.0) / (1.0 - L * 0.4285714) + 1.0;\n" +
+//                        "}\n" +
+//                        "float fromOklab(float L) {\n" +
+//                        "  return (L - 1.0) / (1.0 + L * 0.75) + 1.0;\n" +
+//                        "}\n" +
                         "void main()\n" +
                         "{\n" +
                         "  vec4 tgt = texture2D( u_texture, v_texCoords );\n" +
