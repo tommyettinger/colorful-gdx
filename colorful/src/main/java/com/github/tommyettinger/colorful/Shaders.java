da0f5fae19f45cb3cc51b95abd08e6e1b79fe36d
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-11-18 13:33:49 -0800

    Add (de)saturating Batch to Shaders.
    Former-commit-id: f569ffef1b6ce0fc28f93169085d73dcdee5baf0

--- colorful/src/main/java/com/github/tommyettinger/colorful/Shaders.java
+++ colorful/src/main/java/com/github/tommyettinger/colorful/Shaders.java
@@ -1565,6 +1565,64 @@ void main()
                     "                 (lab * lab * lab)," +
                     "                 0.0, 1.0)), tgt.a);\n" +
                     "}";
+
+    /**
+     * Just like {@link #fragmentShaderInvertedChroma}, but instead of inverting chroma, this tries to significantly
+     * increase the saturation of a color (by pushing chroma away from gray). This works internally by multiplying the A
+     * and B components of the color (as Oklab) by 2.0 . This uses an RGBA batch color.
+     * <br>
+     * You can generate RGB colors using any of various methods in the {@code rgb} package, such as
+     * {@link com.github.tommyettinger.colorful.rgb.ColorTools#rgb(float, float, float, float)}.
+     * <br>
+     * Meant for use with {@link #vertexShader}.
+     */
+    public static String fragmentShaderDoubleSaturation =
+            "#ifdef GL_ES\n" +
+                    "#define LOWP lowp\n" +
+                    "precision mediump float;\n" +
+                    "#else\n" +
+                    "#define LOWP \n" +
+                    "#endif\n" +
+                    "varying vec2 v_texCoords;\n" +
+                    "varying LOWP vec4 v_color;\n" +
+                    "uniform sampler2D u_texture;\n" +
+                    "const vec3 forward = vec3(1.0 / 3.0);\n" +
+                    "void main()\n" +
+                    "{\n" +
+                    "  vec4 tgt = texture2D( u_texture, v_texCoords ) * v_color;\n" +
+                    "  vec3 lab = mat3(+0.2104542553, +1.9779984951, +0.0259040371, +0.7936177850, -2.4285922050, +0.7827717662, -0.0040720468, +0.4505937099, -0.8086757660) *" +
+                    "             pow(mat3(0.4121656120, 0.2118591070, 0.0883097947, 0.5362752080, 0.6807189584, 0.2818474174, 0.0514575653, 0.1074065790, 0.6302613616) \n" +
+                    "             * (tgt.rgb * tgt.rgb), forward);\n" +
+                    "  lab.yz *=   2.000  ;\n" +
+                    "  lab = mat3(1.0, 1.0, 1.0, +0.3963377774, -0.1055613458, -0.0894841775, +0.2158037573, -0.0638541728, -1.2914855480) * lab;\n" +
+                    "  gl_FragColor = vec4(sqrt(clamp(" +
+                    "                 mat3(+4.0767245293, -1.2681437731, -0.0041119885, -3.3072168827, +2.6093323231, -0.7034763098, +0.2307590544, -0.3411344290, +1.7068625689) *\n" +
+                    "                 (lab * lab * lab)," +
+                    "                 0.0, 1.0)), tgt.a);\n" +
+                    "}";
+
+    /**
+     * Prepares and returns a new SpriteBatch that uses {@link #vertexShader} and {@link #fragmentShaderDoubleSaturation}
+     * from this class, making it able to render RGBA colors from the libGDX or the rgb package. This also takes a
+     * {@code saturationMultiplier} parameter, which modifies the saturation in the fragment shader so it doesn't have
+     * to always double the saturation, and can change it by more or less. If greater than 1.0, saturationMultiplier
+     * will make colors more saturated/rich/vivid, while if it is less than 1.0 it will make all colors closer to gray.
+     * Using 0.0 will make all colors grayscale.
+     * <br>
+     * You can generate RGB colors using any of various methods in the {@code rgb} package, such as
+     * {@link com.github.tommyettinger.colorful.rgb.ColorTools#rgb(float, float, float, float)}.
+     *
+     * @param saturationMultiplier what to multiply the saturation by; 1.0 will have no change, 0.5 desaturates, 2.0 over-saturates
+     * @return a freshly allocated SpriteBatch that will also have a new ShaderProgram for rendering RGB with contrast
+     */
+    public static SpriteBatch makeSaturatingBatch(final float saturationMultiplier)
+    {
+        ShaderProgram shader = new ShaderProgram(vertexShader, fragmentShaderDoubleSaturation.replace("   2.0   ", Float.toString(saturationMultiplier)));
+        if(!shader.isCompiled())
+            throw new GdxRuntimeException("Couldn't compile shader: " + shader.getLog());
+        return new SpriteBatch(1000, shader);
+    }
+
     /**
      * This is similar to the default vertex shader from libGDX, but also does some expensive setup work for HSLuv batch
      * colors per-vertex instead of per-fragment. It is needed if you use {@link #fragmentShaderHsluv}.
