56514deea36755cd339bcd78b4150a9d69774b78
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-12-12 17:08:24 -0800

    The shader to swap definitely works, but...
    The gradients sample seems to have some lossiness affecting its white pixels, with only a handful actually pure white.
    
    
    Former-commit-id: 71e654ffe44aa6af6a2805f786b02950767c74fd

--- colorful/src/test/java/com/github/tommyettinger/colorful/HardReplaceDemo.java
+++ colorful/src/test/java/com/github/tommyettinger/colorful/HardReplaceDemo.java
@@ -3,51 +3,44 @@ package com.github.tommyettinger.colorful;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3WindowAdapter;
 import com.badlogic.gdx.files.FileHandle;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.Colors;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShaderProgram;
-import com.badlogic.gdx.math.RandomXS128;
-import com.badlogic.gdx.utils.OrderedMap;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.scenes.scene2d.utils.UIUtils;
+import com.badlogic.gdx.utils.TimeUtils;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 import static com.badlogic.gdx.Gdx.input;
 
-public class ColorReplacementDemo extends ApplicationAdapter {
-    //public static final int backgroundColor = Color.rgba8888(Color.DARK_GRAY);
-//    public static final int SCREEN_WIDTH = 1531;
-//    public static final int SCREEN_HEIGHT = 862;
-    public static final int SCREEN_WIDTH = 808;
-    public static final int SCREEN_HEIGHT = 600;
+public class HardReplaceDemo extends ApplicationAdapter {
+    public static final int SCREEN_WIDTH = 512;
+    public static final int SCREEN_HEIGHT = 224;
     protected SpriteBatch batch;
+    protected SpriteBatch colorizeBatch;
     protected Viewport screenView;
     protected Texture screenTexture;
 
     private long lastProcessedTime = 0L;
-    private ShaderProgram defaultShader;
-    private ShaderProgram shader;
-    
-    private OrderedMap<String, Color> colors;
-    private RandomXS128 random;
-    private Color search, replace;
+    private float r = 1f;
+    private float g = 1f;
+    private float b = 1f;
 
     public static void main(String[] arg) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-        config.setTitle("Replacement Demo");
+        config.setTitle("Swap White Demo");
         config.setWindowedMode(SCREEN_WIDTH, SCREEN_HEIGHT);
         config.setIdleFPS(10);
         config.useVsync(true);
 //        config.setResizable(false);
 
-        final ColorReplacementDemo app = new ColorReplacementDemo();
+        final HardReplaceDemo app = new HardReplaceDemo();
         config.setWindowListener(new Lwjgl3WindowAdapter() {
             @Override
             public void filesDropped(String[] files) {
@@ -79,75 +72,46 @@ public class ColorReplacementDemo extends ApplicationAdapter {
         screenView.update(width, height);
         screenView.getCamera().position.set(width * 0.5f, height * 0.5f, 0f);
     }
-    
-    private void shuffle(){
-        Object[] items = colors.orderedKeys().items;
-        for (int i = colors.orderedKeys().size - 1; i >= 0; i--) {
-            int ii = random.nextInt(i+1);
-            Object temp = items[i];
-            items[i] = items[ii];
-            items[ii] = temp;
-        }
-    }
 
     @Override
     public void create() {
-        random = new RandomXS128(0x123456789ABCDEFL, 0xB0BAFE77);
-        colors = new OrderedMap<>(Colors.getColors().size);
-        for(String k : Colors.getColors().keys())
-            colors.put(k, Colors.get(k));
-        colors.orderedKeys().sort();
-        shuffle();
-        defaultShader = SpriteBatch.createDefaultShader();
-        shader = new ShaderProgram(Shaders.vertexShader, Shaders.fragmentShaderReplacement);
-        if(!shader.isCompiled())
-            System.out.println(shader.getLog());
-        batch = new SpriteBatch(8000, defaultShader);
+        batch = new SpriteBatch();
+        ShaderProgram shader = new ShaderProgram(Shaders.vertexShader, Shaders.fragmentShaderInvertedRGB);
+        if(!shader.isCompiled()){
+            System.out.println("Shader failed to compile: " + shader.getLog());
+            shader = null;
+        }
+        colorizeBatch = Shaders.makeSwappingBatch(1f, 0f, 0f);
         screenView = new ScreenViewport();
         screenView.getCamera().position.set(SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f, 0);
         screenView.update(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         batch.enableBlending();
-        
-        input.setInputProcessor(new InputAdapter(){
-            @Override
-            public boolean keyUp(int keycode) {
-                switch (keycode){
-                    case Input.Keys.Q:
-                    case Input.Keys.ESCAPE:
-                        Gdx.app.exit();
-                        return true;
-                    default:
-                        shuffle();
-                        System.out.println("Searching for " + colors.orderedKeys().get(0) + ", replacing with " + colors.orderedKeys().get(1));
-                        return true;
-                }
-            }
-        });
-        
+
         // if you don't have these files on this absolute path, that's fine, and they will be ignored
 //        load("samples/Painting_by_Henri_Biva.jpg");
 //        load("samples/Among_the_Sierra_Nevada_by_Albert_Bierstadt.jpg");
-        load("samples/Mona_Lisa.jpg");
+        load("samples/gradients.png");
     }
 
 
     @Override
     public void render() {
-        Gdx.gl.glClearColor(0.4f, 0.5f, 0.8f, 1f);
+        Gdx.gl.glClearColor(0.4f, 0.4f, 0.4f, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        handleInput();
         batch.setProjectionMatrix(screenView.getCamera().combined);
+        colorizeBatch.setProjectionMatrix(screenView.getCamera().combined);
         if (screenTexture != null) {
-            batch.setShader(shader);
+            colorizeBatch.setColor(r, g, b, 1f);
+            colorizeBatch.begin();
+            colorizeBatch.draw(screenTexture, 0, 0);
+            colorizeBatch.end();
             batch.begin();
-            shader.setUniformf("u_search", colors.get(colors.orderedKeys().get(0)));
-            shader.setUniformf("u_replace", colors.get(colors.orderedKeys().get(1)));
-            batch.draw(screenTexture, 0, 0);
-            batch.setShader(defaultShader);
             batch.setPackedColor(-0x1.fffffep126f); // packed white
             batch.draw(screenTexture, screenTexture.getWidth(), 0);
             batch.end();
         }
-        
+
     }
 
     @Override
@@ -156,4 +120,45 @@ public class ColorReplacementDemo extends ApplicationAdapter {
         screenView.getCamera().position.set(width * 0.5f, height * 0.5f, 0f);
         screenView.getCamera().update();
     }
+
+    public void handleInput() {
+        if (input.isKeyPressed(Input.Keys.V)) // view
+            System.out.println("r=" + r + ",g=" + g + ",b=" + b);
+        else if (input.isKeyPressed(Input.Keys.D))
+            load("samples/Dawnlike.png");
+        else if (input.isKeyPressed(Input.Keys.A)) // abstract gradient
+            load("samples/gradients.png");
+        else if (input.isKeyPressed(Input.Keys.M))
+            load("samples/Mona_Lisa.jpg");
+        else if (input.isKeyPressed(Input.Keys.S)) //Sierra Nevada
+            load("samples/Among_the_Sierra_Nevada_by_Albert_Bierstadt.jpg");
+        else if (input.isKeyPressed(Input.Keys.P)) // Pond
+            load("samples/Painting_by_Henri_Biva.jpg");
+        else if (input.isKeyPressed(Input.Keys.W)) // Wargame, pixel art cartoon style
+            load("samples/Color_Guard.png");
+        else if (input.isKeyPressed(Input.Keys.O)) // old-style grayscale palette
+            load("samples/Grayscale_Spaceships.png");
+        else if (input.isKeyPressed(Input.Keys.Q) || input.isKeyPressed(Input.Keys.ESCAPE)) //quit
+            Gdx.app.exit();
+        else {
+            // only process once every 150 ms, or just over 6 times a second, at most
+            if (TimeUtils.timeSinceMillis(lastProcessedTime) < 150)
+                return;
+            lastProcessedTime = TimeUtils.millis();
+            if (input.isKeyPressed(Input.Keys.R)) //red
+                r = MathUtils.clamp(r + (UIUtils.shift() ? -0x3p-7f : 0x3p-7f), 0f, 1f);
+            else if (input.isKeyPressed(Input.Keys.G)) //green
+                g = MathUtils.clamp(g + (UIUtils.shift() ? -0x3p-8f : 0x3p-8f), 0f, 1f);
+            else if (input.isKeyPressed(Input.Keys.B)) //blue
+                b = MathUtils.clamp(b + (UIUtils.shift() ? -0x3p-8f : 0x3p-8f), 0f, 1f);
+            else if (input.isKeyPressed(Input.Keys.Z)) // zero changes
+            {
+                r = 1f;
+                g = 1f;
+                b = 1f;
+            }
+        }
+    }
+
 }
+
