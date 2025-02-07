287a32802c40482a19319ddbfe59d9ba8283464b
Tommy Ettinger <tommy.ettinger@gmail.com>
2025-02-06 23:20:01 -0800

    Improve HslCorrectnessTest a lot.

--- colorful/src/test/java/com/github/tommyettinger/colorful/HSLCorrectnessTest.java
+++ colorful/src/test/java/com/github/tommyettinger/colorful/HSLCorrectnessTest.java
@@ -21,6 +21,7 @@ import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.utils.IntMap;
 import com.badlogic.gdx.utils.NumberUtils;
 import com.github.tommyettinger.colorful.rgb.ColorTools;
+import org.junit.Test;
 
 public class HSLCorrectnessTest {
 
@@ -77,53 +78,57 @@ public class HSLCorrectnessTest {
         return Color.toFloatBits(hue, sat, lit, a);
     }
 
+    int[] colors = {
+            0xFFFFFFFF,
+            0x808080FF,
+            0x000000FF,
+            0xFF0000FF,
+            0xBFBF00FF,
+            0x008000FF,
+            0x80FFFFFF,
+            0x8080FFFF,
+            0xBF40BFFF,
+            0xA0A424FF,
+            0x411BEAFF,
+            0x1EAC41FF,
+            0xF0C80EFF,
+            0xB430E5FF,
+            0xED7651FF,
+            0xFEF888FF,
+            0x19CB97FF,
+            0x362698FF,
+            0x7E7EB8FF,
+    };
+
+    IntMap<float[]> map = new IntMap<>(19);
+
+    {
+        map.put(0xFFFFFFFF, new float[]{0.000f, 0.000f, 1.000f});
+        map.put(0x808080FF, new float[]{0.000f, 0.000f, 0.500f});
+        map.put(0x000000FF, new float[]{0.000f, 0.000f, 0.000f});
+        map.put(0xFF0000FF, new float[]{0.000f, 1.000f, 0.500f});
+        map.put(0xBFBF00FF, new float[]{60.00f, 1.000f, 0.375f});
+        map.put(0x008000FF, new float[]{120.0f, 1.000f, 0.250f});
+        map.put(0x80FFFFFF, new float[]{180.0f, 1.000f, 0.750f});
+        map.put(0x8080FFFF, new float[]{240.0f, 1.000f, 0.750f});
+        map.put(0xBF40BFFF, new float[]{300.0f, 0.500f, 0.500f});
+        map.put(0xA0A424FF, new float[]{61.80f, 0.638f, 0.393f});
+        map.put(0x411BEAFF, new float[]{251.1f, 0.832f, 0.511f});
+        map.put(0x1EAC41FF, new float[]{134.9f, 0.707f, 0.396f});
+        map.put(0xF0C80EFF, new float[]{49.50f, 0.893f, 0.497f});
+        map.put(0xB430E5FF, new float[]{283.7f, 0.775f, 0.542f});
+        map.put(0xED7651FF, new float[]{14.30f, 0.817f, 0.624f});
+        map.put(0xFEF888FF, new float[]{56.90f, 0.991f, 0.765f});
+        map.put(0x19CB97FF, new float[]{162.4f, 0.779f, 0.447f});
+        map.put(0x362698FF, new float[]{248.3f, 0.601f, 0.373f});
+        map.put(0x7E7EB8FF, new float[]{240.5f, 0.290f, 0.607f});
+    }
+
     /**
      * Cross-reference with <a href="https://en.wikipedia.org/wiki/HSL_and_HSV#Examples">Wikipedia - HSL and HSV</a>.
-     * @param args
      */
-    public static void main(String[] args) {
-        int[] colors = {
-                0xFFFFFFFF,
-                0x808080FF,
-                0x000000FF,
-                0xFF0000FF,
-                0xBFBF00FF,
-                0x008000FF,
-                0x80FFFFFF,
-                0x8080FFFF,
-                0xBF40BFFF,
-                0xA0A424FF,
-                0x411BEAFF,
-                0x1EAC41FF,
-                0xF0C80EFF,
-                0xB430E5FF,
-                0xED7651FF,
-                0xFEF888FF,
-                0x19CB97FF,
-                0x362698FF,
-                0x7E7EB8FF,
-        };
-
-        IntMap<float[]> map = new IntMap<>(19);
-        map.put(0xFFFFFFFF,	new float[]{0.000f,	0.000f, 1.000f});
-        map.put(0x808080FF,	new float[]{0.000f,	0.000f, 0.500f});
-        map.put(0x000000FF,	new float[]{0.000f,	0.000f, 0.000f});
-        map.put(0xFF0000FF,	new float[]{0.000f,	1.000f, 0.500f});
-        map.put(0xBFBF00FF,	new float[]{60.00f,	1.000f, 0.375f});
-        map.put(0x008000FF,	new float[]{120.0f,	1.000f, 0.250f});
-        map.put(0x80FFFFFF,	new float[]{180.0f,	1.000f, 0.750f});
-        map.put(0x8080FFFF,	new float[]{240.0f,	1.000f, 0.750f});
-        map.put(0xBF40BFFF,	new float[]{300.0f,	0.500f, 0.500f});
-        map.put(0xA0A424FF,	new float[]{61.80f,	0.638f, 0.393f});
-        map.put(0x411BEAFF,	new float[]{251.1f,	0.832f, 0.511f});
-        map.put(0x1EAC41FF,	new float[]{134.9f,	0.707f, 0.396f});
-        map.put(0xF0C80EFF,	new float[]{49.50f,	0.893f, 0.497f});
-        map.put(0xB430E5FF,	new float[]{283.7f,	0.775f, 0.542f});
-        map.put(0xED7651FF,	new float[]{14.30f,	0.817f, 0.624f});
-        map.put(0xFEF888FF,	new float[]{56.90f,	0.991f, 0.765f});
-        map.put(0x19CB97FF,	new float[]{162.4f,	0.779f, 0.447f});
-        map.put(0x362698FF,	new float[]{248.3f,	0.601f, 0.373f});
-        map.put(0x7E7EB8FF,	new float[]{240.5f,	0.290f, 0.607f});
+    @Test
+    public void testRgb2hslRogue() {
         float hAbs = 0f, hRel = 0f, hMax = 0f;
         float sAbs = 0f, sRel = 0f, sMax = 0f;
         float lAbs = 0f, lRel = 0f, lMax = 0f;
@@ -141,8 +146,75 @@ public class HSLCorrectnessTest {
             err = lit - target[2]; lMax = Math.max(Math.abs(err), lMax); lRel += err; lAbs += Math.abs(err);
         }
 
+        System.out.println("rgb2hslRogue :");
         System.out.printf("Hue: Relative error %8.6f, absolute error %8.6f, max error %8.6f\n", hRel, hAbs, hMax);
         System.out.printf("Sat: Relative error %8.6f, absolute error %8.6f, max error %8.6f\n", sRel, sAbs, sMax);
         System.out.printf("Lit: Relative error %8.6f, absolute error %8.6f, max error %8.6f\n", lRel, lAbs, lMax);
+    }
+
+    /**
+     * Cross-reference with <a href="https://en.wikipedia.org/wiki/HSL_and_HSV#Examples">Wikipedia - HSL and HSV</a>.
+     */
+    @Test
+    public void testRgb2hsl() {
+        float hAbs = 0f, hRel = 0f, hMax = 0f;
+        float sAbs = 0f, sRel = 0f, sMax = 0f;
+        float lAbs = 0f, lRel = 0f, lMax = 0f;
+        for (int c : colors) {
+            float[] target = map.get(c);
+            float abgr = ColorTools.fromRGBA8888(c);
+            float hsla = FloatColors.rgb2hsl(abgr);
+            float hue = ColorTools.red(hsla) * 360f;
+            float sat = ColorTools.green(hsla);
+            float lit = ColorTools.blue(hsla);
+            float err;
+            err = hue - target[0]; hMax = Math.max(Math.abs(err), hMax); hRel += err; hAbs += Math.abs(err);
+            System.out.printf("0x%08X: %7.3f %4.3f %4.3f with hue error %5.4f\n", c, hue, sat, lit, err);
+            err = sat - target[1]; sMax = Math.max(Math.abs(err), sMax); sRel += err; sAbs += Math.abs(err);
+            err = lit - target[2]; lMax = Math.max(Math.abs(err), lMax); lRel += err; lAbs += Math.abs(err);
+        }
+
+        System.out.println("rgb2hsl :");
+        System.out.printf("Hue: Relative error %8.6f, absolute error %8.6f, max error %8.6f\n", hRel, hAbs, hMax);
+        System.out.printf("Sat: Relative error %8.6f, absolute error %8.6f, max error %8.6f\n", sRel, sAbs, sMax);
+        System.out.printf("Lit: Relative error %8.6f, absolute error %8.6f, max error %8.6f\n", lRel, lAbs, lMax);
+    }
+
+    /**
+     * Cross-reference with <a href="https://en.wikipedia.org/wiki/HSL_and_HSV#Examples">Wikipedia - HSL and HSV</a>.
+     */
+    @Test
+    public void testHsl2rgb() {
+        float rAbs = 0f, rMax = 0f;
+        float gAbs = 0f, gMax = 0f;
+        float bAbs = 0f, bMax = 0f;
+        double rRel = 0.0;
+        double gRel = 0.0;
+        double bRel = 0.0;
+
+        for (int c : colors) {
+            float tr = (c>>>24&255) / 255f;
+            float tg = (c>>>16&255) / 255f;
+            float tb = (c>>> 8&255) / 255f;
+            float[] target = map.get(c);
+            float rgba = FloatColors.hsl2rgb(target[0] / 360f, target[1], target[2], 1f);
+            float r = ColorTools.red(rgba);
+            float g = ColorTools.green(rgba);
+            float b = ColorTools.blue(rgba);
+            float err;
+            System.out.printf("0x%08X: %4.3f %4.3f %4.3f, should be %4.3f %4.3f %4.3f\n", c, r, g, b, tr, tg, tb);
+            err = r - tr; rMax = Math.max(Math.abs(err), rMax); rAbs += Math.abs(err);
+            err = g - tg; gMax = Math.max(Math.abs(err), gMax); gAbs += Math.abs(err);
+            err = b - tb; bMax = Math.max(Math.abs(err), bMax); bAbs += Math.abs(err);
+        }
+
+        rRel = Math.sqrt(rAbs * rAbs / colors.length);
+        gRel = Math.sqrt(gAbs * gAbs / colors.length);
+        bRel = Math.sqrt(bAbs * bAbs / colors.length);
+
+        System.out.println("hsl2rgb :");
+        System.out.printf("r: RMS error %8.6f, absolute error %8.6f, max error %8.6f\n", rRel, rAbs, rMax);
+        System.out.printf("g: RMS error %8.6f, absolute error %8.6f, max error %8.6f\n", gRel, gAbs, gMax);
+        System.out.printf("b: RMS error %8.6f, absolute error %8.6f, max error %8.6f\n", bRel, bAbs, bMax);
     }
 }
